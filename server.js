d6d359c5ac4211436664cce0aad93ee421523daa
Parr <tedblount01@gmail.com>
2020-06-04 23:42:08 -0500

    wtf

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
-
+const bodyParser = require('body-parser')
 const mongoose = require("mongoose");
+const methodOverride = require('method-override');
 // const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
